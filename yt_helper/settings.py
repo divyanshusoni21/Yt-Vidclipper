1acc32906815adb7fa7d5ef469452cca94586148
Divyanshu soni <divyanshusoni.relish@gmail.com>
2026-01-04 23:30:49 +0530

    changes to prevent ytdlp bot detection

--- yt_helper/settings.py
+++ yt_helper/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 DEBUG = os.environ.get("DEBUG", False)
 
 
-ALLOWED_HOSTS = ["127.0.0.1","64.227.185.227"]
+ALLOWED_HOSTS = ["127.0.0.1","64.227.185.227","64.227.185.227.nip.io"]
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 
